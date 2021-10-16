f68a59063ba11ff614c7c78374c25fe85f7512d5
s-moe <sarahannmoe@gmail.com>
2021-10-16 11:48:46 -0700

    rendered modal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <h1>{{title}}</h1>
   <input type="text" ref="name">
-  <button @click="handleClick">Click Me</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 
 export default {
   name: "App",
+  components: {Modal},
   data() {
     return {
       title: 'My first vue app'
